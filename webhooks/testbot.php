87a2af5995e802667c8e5958f5fb6ae1ceabae75
Jhonatan Holguin <jholguin9004@gmail.com>
2019-03-21 03:06:39 -0500

    index

--- webhooks/testbot.php
+++ webhooks/testbot.php
@@ -4,7 +4,9 @@ include_once 'lib/somosioticos_dialogflow.php';
 credenciales('jholguin9004', 'Sofi789456');
 if(intent_recibido('prueba')){
 	$params = obtener_variables();
-	var_dump($params);
+	if(isset($params['personas'])){
+		enviar_texto(implode(', ', $params['personas']) . 'jugaron');
+	}
 }
 //
 //
